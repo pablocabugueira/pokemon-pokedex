33a9375aa4e8fa07917b3cbcd5d1e4f67a836442
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2023-04-27 00:55:07 -0300

    added previous and next buttons

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const pokemonImg = document.querySelector("#pokemonImg")
 const form = document.querySelector("#form")
 const inputSearch = document.querySelector("#inputSearch")
 
+const prevButton = document.querySelector("#prevButton")
+const nextButton = document.querySelector("#nextButton")
+
 let pokemonIdSearch = 1 
 
 const fetchPokemon = async (pokemon) => {
@@ -22,12 +25,13 @@ const renderPokemon = async (pokemon) => {
 
     const data = await fetchPokemon(pokemon)
 
-    if (data) {
+    if (data, data.id < 650) {
         pokemonNumber.innerHTML = data.id
         pokemonName.innerHTML = data.name
         pokemonImg.style.display = ""
         pokemonImg.src = data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"]
         inputSearch.value = ""
+        pokemonIdSearch = data.id
     } else {
         pokemonNumber.innerHTML = ""
         pokemonName.innerHTML = "Not found :c"
@@ -38,6 +42,20 @@ const renderPokemon = async (pokemon) => {
 form.addEventListener("submit", (event) => {
     event.preventDefault()
     renderPokemon(inputSearch.value.toLowerCase())
+})
+
+prevButton.addEventListener("click", () => {
+    if (pokemonIdSearch > 1) {
+        pokemonIdSearch -= 1
+        renderPokemon(pokemonIdSearch)
+    }
+})
+
+nextButton.addEventListener("click", () => {
+    if (pokemonIdSearch < 649) {
+        pokemonIdSearch += 1
+        renderPokemon(pokemonIdSearch)
+    }
 })
 
 renderPokemon(pokemonIdSearch)
